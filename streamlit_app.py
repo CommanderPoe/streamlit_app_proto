6294a9ba7fd185174b1ae8e2f24cd57d655b7ff2
Enne <33594578+CommanderPoe@users.noreply.github.com>
2023-03-23 20:55:20 +0100

    Update streamlit_app.py
    adding widget

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
+# importing libraries
 import streamlit
 import pandas
 
+# reading csv in s3 bucket
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+# adding title and text for menu
 streamlit.title('Dinner Chicken Dinner')
 
 streamlit.header('Mornning Menu')
@@ -12,4 +15,8 @@ streamlit.text('🍞 Bread with butten and ham')
 
 streamlit.header('Smoothies, make your own 🥝')
 
+# adding widget to select fruits
+streamlit.multiselect('Pick your own:', list(my_fruit_list.index))
+
+# printing dataframe
 streamlit.dataframe(my_fruit_list)
