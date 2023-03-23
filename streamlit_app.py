20c1f0005df8eb021c600b0faa61913f9109000b
Enne <33594578+CommanderPoe@users.noreply.github.com>
2023-03-23 20:48:00 +0100

    Update streamlit_app.py
    added emojies and second header

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit
 streamlit.title('Dinner Chicken Dinner')
 
 streamlit.header('Mornning Menu')
-streamlit.text('Eggs with potato and bread')
-streamlit.text('Tomatoes, tonno and mozarella')
-streamlit.text('Bread with butten and ham')
+streamlit.text('🍳 Eggs with potato and bread')
+streamlit.text('🥗 Tomatoes, tonno and mozarella')
+streamlit.text('🍞 Bread with butten and ham')
+
+streamlit.header('Smoothies, make your own 🥝')
