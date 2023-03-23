7599349d34f38fdeeaae1a023389d472f8212a79
Enne <33594578+CommanderPoe@users.noreply.github.com>
2023-03-23 20:44:14 +0100

    Update streamlit_app.py
    added morning menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title('Dinner Dinner Chicken')
+streamlit.title('Dinner Chicken Dinner')
+
+streamlit.header('Mornning Menu')
+streamlit.text('Eggs with potato and bread')
+streamlit.text('Tomatoes, tonno and mozarella')
+streamlit.text('Bread with butten and ham')
