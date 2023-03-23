18ae350d776dc5b6949b77b51e0859d17b1a0b8a
Enne <33594578+CommanderPoe@users.noreply.github.com>
2023-03-23 20:51:08 +0100

    Update streamlit_app.py
    added pandas and created table

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('Dinner Chicken Dinner')
 
@@ -8,3 +11,5 @@ streamlit.text('🥗 Tomatoes, tonno and mozarella')
 streamlit.text('🍞 Bread with butten and ham')
 
 streamlit.header('Smoothies, make your own 🥝')
+
+streamlit.dataframe(my_fruit_list)
